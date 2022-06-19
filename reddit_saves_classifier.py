df5bba44e23e7cf48d440a4237b65463d6616bf7
austinmaciey <amaciey@gmail.com>
2022-06-19 15:06:32 -0700

    Files are created and links are added. Script is funcional in the most basic sense

--- reddit_saves_classifier.py
+++ reddit_saves_classifier.py
@@ -7,29 +7,38 @@ import praw
 
 reddit = praw.Reddit("bot1")
 
-# TODO: Create static path to local directory for files created by this script
 user_txt_file_path = '/Users/austinmaciey/Documents/'
-print(os.listdir(user_txt_file_path))
 
 if not os.path.exists(user_txt_file_path + 'Saved Reddit Posts'):
     os.mkdir(user_txt_file_path + 'Saved Reddit Posts')
 
-print(os.listdir(user_txt_file_path))
-
 # Creates praw ListGenerator object containing post ids of all saved posts by the user
-saved_posts = reddit.user.me().saved(limit=5)
+saved_posts = reddit.user.me().saved(limit=1000)
 
 # Iterates through saved posts, prints out the subreddit, and un-saves posts which have been deleted
 for i in saved_posts:
     try:
-        print(reddit.submission(id=str(i)).subreddit)
+        if not os.path.exists(
+                user_txt_file_path + 'Saved Reddit Posts/' + str(reddit.submission(id=str(i)).subreddit) + ".html"):
+            with open(
+                    user_txt_file_path + 'Saved Reddit Posts/' + str(reddit.submission(id=str(i)).subreddit) + ".html",
+                    'w') as f:
+                f.writelines(
+                    f'<a href="{reddit.submission(id=str(i)).shortlink}">{reddit.submission(id=str(i)).title}</a>')
+                f.close()
+
+        else:
+            with open(
+                    user_txt_file_path + 'Saved Reddit Posts/' + str(reddit.submission(id=str(i)).subreddit) + ".html",
+                    'a') as f:
+                # TODO: search for the link or title in the html and only add it if not already present
+                f.writelines(
+                    f'<br/> <a href="{reddit.submission(id=str(i)).shortlink}">{reddit.submission(id=str(i)).title}</a>')
+                f.close()
 
     except:
         print("That Post has been deleted")
-        reddit.submission(id=str(i)).unsave()
-
-
-# TODO: write function to create .txt files for each unique subreddit and add links and information on each post to those txt files
+        #reddit.submission(id=str(i)).unsave()
 
 # TODO: write a function to read .txt files and look for flags to unsave posts
 
